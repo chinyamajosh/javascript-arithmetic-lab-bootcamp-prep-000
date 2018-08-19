24a57c0ad03edbad8ce6214c434b546dd5961515
Joshua Chinyama <chinyamajosh@gmail.com>
2018-08-19 09:22:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function add(x, y) {
+  return x + y
+}
+
+add(1, 80)
+
+function subtract(x, y) {
+  return x - y
+}
+
+subtract(60, 40)
+
+function multiply(x, y) {
+  return x * y
+}
+
+multiply(2, 3.4)
